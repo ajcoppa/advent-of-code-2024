b49e223e5aad7ba0eb7f1de1c72dcb537c6c9435
AJ Coppa <aj.coppa@gmail.com>
2024-12-03 09:46:17 -0500

    03**

--- 03.ts
+++ 03.ts
@@ -2,17 +2,27 @@ import { loadFromFile } from "./lib.ts";
 
 async function main() {
   const lines = await loadFromFile("./03-input.txt");
-  const instructions = parseInstructions(lines);
-  console.log(`Part 1: ${partOne(instructions)}`); 
+  console.log(`Part 1: ${partOne(lines)}`); 
+  console.log(`Part 2: ${partTwo(lines)}`); 
 }
 
-function partOne(instructions: number[][]) {
-  return instructions.reduce((acc, [x, y]) => {
+function partOne(lines: string[]) {
+  return parseInstructionsOne(lines).reduce((acc, [x, y]) => {
     return acc + x * y;
   }, 0);
 }
 
-function parseInstructions(lines: string[]) {
+function partTwo(lines: string[]) {
+  const result = parseInstructionsTwo(lines).reduce((acc, instruction) => (
+    instruction === "don't" ? {enabled: false, count: acc.count} :
+    instruction === "do" ? {enabled: true, count: acc.count} :
+    acc.enabled ? {enabled: acc.enabled, count: acc.count + instruction[0] * instruction[1]} :
+    acc
+  ), {count: 0, enabled: true});
+  return result.count;
+}
+
+function parseInstructionsOne(lines: string[]): number[][] {
   return lines.map((line) => {
     const matches = [...line.matchAll(/mul\((\d{1,3})\,(\d{1,3})\)/g)];
     return matches.map((match) => {
@@ -20,5 +30,20 @@ function parseInstructions(lines: string[]) {
     });
   }).flat();
 }
+
+function parseInstructionsTwo(lines: string[]): Instruction[] {
+  return lines.map((line) => {
+    const matches = [
+      ...line.matchAll(/mul\((\d{1,3})\,(\d{1,3})\)|(don\'t\(\))|(do\(\))/g),
+    ];
+    return matches.map((match) => {
+      return match[0] === "do()" ? "do" :
+        match[0] === "don't()" ? "don't" :
+        [parseInt(match[1], 10), parseInt(match[2], 10)];
+    });
+  }).flat();
+}
+
+type Instruction = "do" | "don't" | number[];
 
 main();
