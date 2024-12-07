0b0285d9e9a37e94cbb8fe39157f96feea04ad5f
AJ Coppa <aj.coppa@gmail.com>
2024-12-06 22:37:41 -0500

    06**
    This is very slow -- will have to optimize further

--- 06.ts
+++ 06.ts
@@ -10,24 +10,63 @@ import {
 
 async function main() {
   const lines = await loadFromFile("./06-input.txt");
-  const grid = parseGrid(lines);
-  console.log(`Part 1: ${partOne(grid)}`);
+  console.log(`Part 1: ${partOne(parseGrid(lines))}`);
+  console.log(`Part 2: ${partTwo(parseGrid(lines))}`);
 }
 
 function partOne(grid: Grid<Tile>): number {
-  const visited: Grid<boolean> = grid.map((row) => row.map((col) =>
-    col._type === "Guard" ? true : false
+  const visited: Grid<Visited> = grid.map((row) => row.map((col) =>
+    col._type === "Guard" ?
+      { _type: "Visited", going: [col.direction] } :
+      { _type: "NeverVisited" }
   ));
-  while (tick(grid, visited)) {
+  while (tick(grid, visited) !== SimulationResult.GuardLeftGrid) {
     // No-op
   }
-  return sum(visited.map((row) => row.filter((col) => col).length));
+  return sum(visited.map((row) => row.filter((col) => col._type === "Visited").length));
 }
 
-function tick(grid: Grid<Tile>, visited: Grid<boolean>): boolean {
+function partTwo(grid: Grid<Tile>): number {
+  const originalGrid = deepCopy(grid);
+  let visited: Grid<Visited> = grid.map((row) => row.map((col) =>
+    col._type === "Guard" ?
+      { _type: "Visited", going: [col.direction] } :
+      { _type: "NeverVisited" }
+  ));
+
+  const loopCoords = grid.map((row, y) => {
+    return row.map((_col, x) => {
+      if (grid[y][x]._type === "Guard") {
+        return false;
+      }
+
+      grid[y][x] = { _type: "Wall" };
+      let result: SimulationResult;
+      while ((result = tick(grid, visited)) === SimulationResult.KeepGoing) {
+        // No-op
+      }
+
+      // Reset the grid
+      grid = deepCopy(originalGrid);
+      visited = grid.map((row) => row.map((col) =>
+        col._type === "Guard" ?
+          { _type: "Visited", going: [col.direction] } :
+          { _type: "NeverVisited" }
+      ));
+      if (result === SimulationResult.LoopDetected) {
+        return true;
+      }
+      return false;
+    });
+  });
+
+  return loopCoords.flat().filter((col) => col).length;
+}
+
+function tick(grid: Grid<Tile>, visited: Grid<Visited>): SimulationResult {
   const guardY: number = grid.findIndex((row) => row.some((col) => col._type === "Guard"));
   const guardX: number = grid[guardY].findIndex((col) => col._type === "Guard");
-  visited[guardY][guardX] = true;
+  
   const facing = grid[guardY][guardX]._type === "Guard" ?
     grid[guardY][guardX].direction :
     Direction.Up;
@@ -37,17 +76,32 @@ function tick(grid: Grid<Tile>, visited: Grid<boolean>): boolean {
     y: guardY + modifier.y,
   };
 
+  visited[guardY][guardX] = {
+    _type: "Visited",
+    going: visited[guardY][guardX]._type === "Visited" ?
+      [...visited[guardY][guardX].going, facing] :
+      [facing],
+  };
+
   if (!inBounds(newGuardCoord.x, newGuardCoord.y, grid[0].length, grid.length)) {
-    return false;
+    return SimulationResult.GuardLeftGrid;
   }
 
-  if (grid[newGuardCoord.y][newGuardCoord.x]._type === "Empty") {
+  if (visited[newGuardCoord.y][newGuardCoord.x]._type === "Visited" &&
+    visited[newGuardCoord.y][newGuardCoord.x].going.includes(facing)) {
+    grid[guardY][guardX] = { _type: "Empty" };
+    grid[newGuardCoord.y][newGuardCoord.x] = {
+      _type: "Guard",
+      direction: facing,
+    };
+    return SimulationResult.LoopDetected;
+  } else if (grid[newGuardCoord.y][newGuardCoord.x]._type === "Empty") {
     grid[guardY][guardX] = { _type: "Empty" };
     grid[newGuardCoord.y][newGuardCoord.x] = { _type: "Guard", direction: facing };
-    return true;
+    return SimulationResult.KeepGoing;
   } else if (grid[newGuardCoord.y][newGuardCoord.x]._type === "Wall") {
     grid[guardY][guardX] = { _type: "Guard", direction: rotateClockwise(facing) };
-    return true;
+    return SimulationResult.KeepGoing;
   }
 
   console.log(grid);
@@ -64,9 +118,23 @@ function charToTile(c: string): Tile {
   }
 }
 
+function deepCopy<T>(xs: T[]): T[] {
+  return JSON.parse(JSON.stringify(xs));
+}
+
 function parseGrid(lines: string[]): Grid<Tile> {
   return lines.map(line => line.split("").map(charToTile));
 }
+
+enum SimulationResult {
+  KeepGoing,
+  GuardLeftGrid,
+  LoopDetected,
+}
+
+type Visited =
+  | { _type: "NeverVisited" }
+  | { _type: "Visited", going: Direction[] };
 
 type Tile =
   | { _type: "Empty" }
