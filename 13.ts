234c8a9c2eceb2b139f7b68068367c57c9cd279a
AJ Coppa <aj.coppa@gmail.com>
2024-12-14 09:21:42 -0500

    13*

---
+++ 13.ts
@@ -0,0 +1,60 @@
+import fs from "node:fs/promises";
+import { sum } from "./lib.ts";
+import { Coord } from "./lib/Coord.ts";
+
+async function main() {
+  const text = await fs.readFile("13-input.txt", { encoding: "utf-8" });
+  const machineStrings = text.split("\n\n");
+  const machines = machineStrings.map(parseMachine);
+  console.log(`Part 1: ${partOne(machines)}`);
+}
+
+function partOne(machines: Machine[]): number {
+  return sum(machines.map((machine) => {
+    const {aPresses, bPresses} = cramersRule(
+      machine.buttons[0].x,
+      machine.buttons[0].y,
+      machine.buttons[1].x,
+      machine.buttons[1].y,
+      machine.prizeCoord.x,
+      machine.prizeCoord.y,
+    );
+
+    // Non-integer number of presses isn't valid here
+    if (Math.floor(aPresses) !== aPresses || Math.floor(bPresses) !== bPresses) {
+      return 0;
+    }
+    return machine.buttons[0].cost * aPresses + machine.buttons[1].cost * bPresses;
+  }));
+}
+
+function cramersRule(aX: number, aY: number, bX: number, bY: number, prizeX: number, prizeY: number) {
+  const det = aX * bY - aY * bX;
+  const aPresses = (prizeX * bY - prizeY * bX) / det;
+  const bPresses = (prizeY * aX - prizeX * aY) / det;
+  return {aPresses, bPresses};
+}
+
+function parseMachine(line: string): Machine {
+  const matches = line.matchAll(/.*A: X\+(\d+), Y\+(\d+)\n.*B: X\+(\d+), Y\+(\d+)\n.*Prize: X=(\d+), Y=(\d+).*/g);
+  for (const match of matches) {
+    const x1 = parseInt(match[1], 10);
+    const y1 = parseInt(match[2], 10);
+    const x2 = parseInt(match[3], 10);
+    const y2 = parseInt(match[4], 10);
+    const prizeX = parseInt(match[5], 10);
+    const prizeY = parseInt(match[6], 10);
+    return {
+      buttons: [{x: x1, y: y1, cost: 3}, {x: x2, y: y2, cost: 1}],
+      prizeCoord: {x: prizeX, y: prizeY},
+    };
+  }
+  throw new Error("No match found");
+}
+
+type Machine = {
+  buttons: {x: number, y: number, cost: number}[];
+  prizeCoord: Coord;
+}
+
+main();
